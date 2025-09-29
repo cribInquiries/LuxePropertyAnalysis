6b3b94ea24a24800156d01653f6c8a138dfd012a
habib@habibilaptop <luxemanagements.info@gmail.com>
2025-09-29 16:59:24 +0930

    fix(vercel): register tsconfig-paths so @ aliases resolve in serverless runtime

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,8 @@
+// Register tsconfig-paths so absolute aliases like '@/...' work in Vercel runtime
+try {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  require('tsconfig-paths/register');
+} catch {}
 import app from '../src/index';
 
 export default app;
